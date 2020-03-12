2f4a77bdc18fb22a1e5650d4fc3f3f8e4070b8a8
Olga Zyk <olga.zyk.sln@gmail.com>
2020-03-12 02:07:35 +0200

    Rename MainPage.php to HomepageController.php; Add homepage.html.twig template; Update base.html.twig;

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class HomepageController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     * @param Request $request
+     * @return Response
+     */
+    public function main(Request $request)
+    {
+
+        return $this->render('homepage/homepage.html.twig');
+    }
+
+}
