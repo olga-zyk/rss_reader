4ac0e65c38bdc7bb84cc676169400127032d751b
Olga Zyk <olga.zyk.sln@gmail.com>
2020-03-14 19:37:12 +0200

    Add SecurityController.php; LoginFormAuthenticator.php; UserIdentifierService.php; login.html.twig template; Update security.yaml; twig.yaml; routes.yaml; Add styles.css to /public directory;

---
+++ src/Service/UserIdentifierService.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Service;
+
+use Symfony\Component\Security\Core\Security;
+
+
+class UserIdentifierService
+{
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        // Avoid calling getUser() in the constructor: auth may not
+        // be complete yet. Instead, store the entire Security object.
+        $this->security = $security;
+    }
+
+    public function someMethod()
+    {
+        // returns User object or null if not authenticated
+        $user = $this->security->getUser();
+    }
+
+}
