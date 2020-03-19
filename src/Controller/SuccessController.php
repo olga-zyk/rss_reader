c6b61de474f81666c6f396f440a1489eab67e7a9
Olga Zyk <37694077+olga-zyk@users.noreply.github.com>
2020-03-19 03:45:26 +0200

    Add SuccessController.php; Add success.html.twig template;

---
+++ src/Controller/SuccessController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SuccessController extends AbstractController
+{
+    /**
+     * @Route("/success", name="success")
+     */
+    public function index()
+    {
+        return $this->render('success/success.html.twig', [
+            'controller_name' => 'SuccessController',
+        ]);
+    }
+}
