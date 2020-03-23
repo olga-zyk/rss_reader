e77076b46809bbe6135cdb35a69ea3a5ac3d19d2
Olga Zyk <37694077+olga-zyk@users.noreply.github.com>
2020-03-23 08:46:28 +0200

    Update composer.json with new packages; Install PHP_CodeSniffer, Add configuration file phpcs.xml.dist; Update .gitignore; Remove empty lines from HomepageController.php;

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -1,13 +1,11 @@
 <?php
 
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
 
 class HomepageController extends AbstractController
 {
@@ -21,5 +19,4 @@ class HomepageController extends AbstractController
 
         return $this->render('homepage/homepage.html.twig');
     }
-
 }
